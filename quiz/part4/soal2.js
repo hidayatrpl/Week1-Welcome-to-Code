712dbfe6cae331c53683a8b3802708ff87782677
hidayatrpl <hidayatrpl1@gmail.com>
2025-04-17 18:38:11 +0700

    push soal 1-3 part 4

---
+++ quiz/part4/soal2.js
@@ -0,0 +1,17 @@
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+function calculateMultiply(nilai1,nilai2){
+    return nilai1 * nilai2;
+}
+
+let num1 = 1
+let num2 = 2
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+// ------------------------------------
+console.log('')
+// -----------------------------------
